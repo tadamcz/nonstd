80e585c6b1501f869072640090a43d5a3dc74290
tadamcz <tadamczewskipublic@gmail.com>
2022-09-19 18:45:03 +0100

    add OneIndexedList

---
+++ nonstd/list.py
@@ -0,0 +1,97 @@
+import collections.abc
+from typing import Iterable, Optional
+
+
+class OneIndexedList(collections.abc.MutableSequence):
+	def __init__(self, init: list = None) -> None:
+		self._wrapped = []
+		if not init is None:
+			self._wrapped.extend(init)
+
+	def _wrapped_index(self, index: [int, slice]) -> [int, slice]:
+		if isinstance(index, slice):
+			return self._wrapped_slice_index(index)
+		if isinstance(index, int):
+			return self._wrapped_integer_index(index)
+
+	def _wrapped_slice_index(self, index: slice) -> slice:
+		start = self._wrapped_integer_index(index.start)
+		stop = self._wrapped_integer_index(index.stop)
+		return slice(start, stop, index.step)
+
+	def _wrapped_integer_index(self, index: [int, None]) -> [int, None]:
+		if index is None:
+			return None
+		if index == 0:
+			raise IndexError(
+				f"This {self.__class__.__name__} is an instance of `OneIndexedList`. Index 0 is forbidden.")
+		if index > 0:
+			return index - 1
+		else:  # In Python, negative indices are used to count backwards from the last element of a list
+			return index
+
+	def __getitem__(self, index: [int, slice]) -> any:
+		wrapped_i = self._wrapped_index(index)
+		return self._wrapped[wrapped_i]
+
+	def __setitem__(self, index: [int, slice], val: any) -> None:
+		wrapped_i = self._wrapped_index(index)
+		self._wrapped[wrapped_i] = val
+
+	def __delitem__(self, index: [int, slice]) -> None:
+		wrapped_i = self._wrapped_index(index)
+		del self._wrapped[wrapped_i]
+
+	def insert(self, index: [int, slice], val: any) -> None:
+		wrapped_i = self._wrapped_index(index)
+		self._wrapped.insert(wrapped_i, val)
+
+	def __len__(self) -> int:
+		return len(self._wrapped)
+
+	def __repr__(self) -> str:
+		return f"{self._wrapped}"
+
+	def __eq__(self, other):
+		if not isinstance(other, OneIndexedList):
+			raise NotImplementedError(f"Cannot compare a `OneIndexedList` to a {other.__class__.__name__}")
+		return self._wrapped == other._wrapped
+
+	def append(self, value: any) -> None:
+		self._wrapped.append(value)
+
+	def extend(self, values: Iterable) -> None:
+		self._wrapped.extend(values)
+
+	def pop(self, index: Optional[int]=None) -> any:
+		if index is None:
+			return self._wrapped.pop()
+		wrapped_i = self._wrapped_index(index)
+		return self._wrapped.pop(wrapped_i)
+
+	def index(self, value: any, start: int=None, stop: int=None) -> int:
+		if start is None:
+			start = 1
+		if stop is None:
+			stop = len(self) + 1
+
+		wrapped_start = self._wrapped_index(start)
+		wrapped_stop = self._wrapped_index(stop)
+		wrapped_i = self._wrapped.index(value, wrapped_start, wrapped_stop)
+
+		return wrapped_i + 1
+
+	def reverse(self) -> None:
+		self._wrapped.reverse()
+
+	def sort(self, key=None, reverse=False):
+		self._wrapped.sort(key=key, reverse=reverse)
+
+	def remove(self, value: any):
+		self._wrapped.remove(value)
+
+	def count(self, value: any) -> int:
+		return self._wrapped.count(value)
+
+	def copy(self):
+		return OneIndexedList(self._wrapped)
---
+++ tests/test_list.py
@@ -0,0 +1,131 @@
+import pytest
+
+from nonstd.list import OneIndexedList
+
+
+class TestOneIndexedList:
+	def test_access(self):
+		oil = OneIndexedList([1, 2])
+		with pytest.raises(IndexError):
+			oil[0]
+		with pytest.raises(IndexError):
+			oil[-3]
+
+		assert oil[1] == 1
+		assert oil[2] == 2
+
+		assert oil[-1] == 2
+		assert oil[-2] == 1
+
+	def test_slicing(self):
+		oil = OneIndexedList([1, 2, 3])
+		assert oil[1:2] == [1]
+		assert oil[:2] == [1]
+
+		assert oil[2:4] == [2, 3]
+		assert oil[2:] == [2, 3]
+
+		assert oil[1:3] == [1, 2]
+
+		assert oil[:4] == [1, 2, 3]
+		assert oil[:1] == []
+
+		assert oil[:4:2] == [1, 3]
+		assert oil[1:4:2] == [1, 3]
+
+		assert oil[-2:] == [2, 3]
+		assert oil[:-2] == [1]
+
+		assert oil[::-1] == [3, 2, 1]
+		assert oil[2::-1] == [2, 1]
+
+	def test_len(self):
+		oil = OneIndexedList()
+		assert len(oil) == 0
+
+		oil = OneIndexedList([1])
+		assert len(oil) == 1
+
+	def test_insert(self):
+		oil = OneIndexedList(["world"])
+		oil.insert(1, "hello")
+		assert oil == OneIndexedList(["hello", "world"])
+
+	def test_equality(self):
+		assert OneIndexedList() == OneIndexedList()
+		assert OneIndexedList([1, 2]) == OneIndexedList([1, 2])
+		assert not OneIndexedList([]) == OneIndexedList([1])
+
+	def test_append(self):
+		oil = OneIndexedList()
+		oil.append(1)
+		assert oil == OneIndexedList([1])
+
+	def test_extend(self):
+		oil = OneIndexedList([1])
+		oil.extend([2, 3])
+		assert oil == OneIndexedList([1, 2, 3])
+
+	def test_pop(self):
+		oil = OneIndexedList([1])
+		assert oil.pop() == 1
+		with pytest.raises(IndexError):
+			oil.pop()
+
+		oil = OneIndexedList([1, 2])
+		assert oil.pop(2) == 2
+
+	def test_index(self):
+		oil = OneIndexedList([1, 2, 3])
+		assert oil.index(1) == 1
+		assert oil.index(2) == 2
+		assert oil.index(3) == 3
+
+		oil = OneIndexedList(["waldo", 0, "waldo"])
+
+		assert oil.index("waldo", start=2) == 3
+		assert oil.index("waldo", start=1) == 1
+		assert oil.index("waldo") == 1
+
+		with pytest.raises(ValueError):
+			oil.index(99)
+
+	def test_clear(self):
+		oil = OneIndexedList([1])
+		oil.clear()
+		assert oil == OneIndexedList()
+		assert len(oil) == 0
+
+	def test_reverse(self):
+		oil = OneIndexedList([1, 2, 3])
+		oil.reverse()
+		assert oil == OneIndexedList([3, 2, 1])
+
+	def test_sort(self):
+		oil = OneIndexedList([2, 1, 3])
+		oil.sort()
+		assert oil == OneIndexedList([1, 2, 3])
+
+		oil.sort(reverse=True)
+		assert oil == OneIndexedList([3, 2, 1])
+
+		oil.sort(key=lambda x: -x)
+		assert oil == OneIndexedList([3, 2, 1])
+
+	def test_remove(self):
+		oil = OneIndexedList([1, 2, 3])
+		oil.remove(1)
+		assert oil == OneIndexedList([2, 3])
+
+		with pytest.raises(ValueError):
+			oil.remove(1)
+
+	def test_count(self):
+		oil = OneIndexedList([1, 2, 3])
+		assert oil.count(3) == 1
+
+	def test_copy(self):
+		oil = OneIndexedList([1])
+		oil2 = oil.copy()
+		del oil
+		assert oil2[1] == 1
